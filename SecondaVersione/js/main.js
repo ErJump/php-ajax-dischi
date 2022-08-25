5dcf660b038a0ad752aae4a827e48257907df57d
ErJump <fedelijunior@gmail.com>
2022-08-25 17:58:59 +0200

    refactor: :zap: Adds if condition to getAlbums for checking genre

--- SecondaVersione/js/main.js
+++ SecondaVersione/js/main.js
@@ -6,23 +6,24 @@ const app = new Vue({
     },
     methods: {
         getAlbums: function() {
-            axios.get('http://localhost/php-ajax-dischi/SecondaVersione/controller.php')
-                .then(response => {
-                    this.albums = response.data;
-                }).catch(error => {
-                    console.log(error);
-                }
-            );
+            if(this.selectedGenre == '') {
+                axios.get('http://localhost/php-ajax-dischi/SecondaVersione/controller.php')
+                    .then(response => {
+                        this.albums = response.data;
+                    }).catch(error => {
+                        console.log(error);
+                    }
+                );
+            } else {
+                axios.get('http://localhost/php-ajax-dischi/SecondaVersione/controller.php?genre=' + this.selectedGenre)
+                    .then(response => {
+                        this.albums = response.data;
+                    }).catch(error => {
+                        console.log(error);
+                    }
+                );
+            }
         },
-        getFilteredAlbums: function() {
-            axios.get('http://localhost/php-ajax-dischi/SecondaVersione/controller.php?genre=' + this.selectedGenre)
-                .then(response => {
-                    this.albums = response.data;
-                }).catch(error => {
-                    console.log(error);
-                }
-            );
-        }   
     },
     created: function() {
         this.getAlbums();
